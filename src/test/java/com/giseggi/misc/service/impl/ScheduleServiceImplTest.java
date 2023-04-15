3c6f45d0bc960895034958aa2e2690c49399b516
kylee <giseggi57@gmail.com>
2023-04-15 15:01:52 +0900

    delete unnecessary import

--- src/test/java/com/giseggi/misc/service/impl/ScheduleServiceImplTest.java
+++ src/test/java/com/giseggi/misc/service/impl/ScheduleServiceImplTest.java
@@ -1,19 +1,9 @@
 package com.giseggi.misc.service.impl;
 
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
 import com.giseggi.misc.service.ScheduleService;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ScheduleServiceImplTest {
@@ -23,7 +13,7 @@ class ScheduleServiceImplTest {
 
 
     @Test
-    void 로그테스트() {
+    void test() {
         testClass.printLog();
     }
 }
